069c068860e5d3be2c0fef2f1106b93534c23312
Akileswaran-Dev <akileswaran.dev@gmail.com>
2026-04-05 13:23:40 +0530

    update file structure- 0504

---
+++ src/app/Pages/Analytics/page.jsx
@@ -0,0 +1,11 @@
+import AppLayout from "../../../components/AppLayout";
+
+export default function AnalyticsPage() {
+  return (
+    <AppLayout>
+      <div style={{ padding: "32px" }}>
+        <h1>Analytics Page</h1>
+      </div>
+    </AppLayout>
+  );
+}
